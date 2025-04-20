3ccabde06a7dae55071265d6dee0bbd1b0f464ea
Dragunov <magorros@gmail.com>
2025-04-19 21:23:32 -0300

    Todo el proyecto

---
+++ app/Views/plantilla.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang = "en">
+
+    <head>
+        <style>
+    body {
+      background-image: url('assets/img/Wallpaper2.jpg');
+      background-size: cover;
+      background-repeat: no-repeat;
+      background-position: center center;
+    }
+        </style>
+        <meta charset= "utf-8">
+        <meta http-equiv="X-UA-Compatible">
+        <meta name= "viewport" content= "width= device-width, initialscale=1, shrink-to-fit=no">
+        <title> Leblanc</title>
+        <link href= "assets/css/bootstrap.min.css" rel="stylesheet" integrity= " " crossorigin="">
+        <link rel="stylesheet" href= "assets/css/estilo.css">
+    </head>
+
+
+    <body>
+        <script src="assets/js/bootstrap.bundle.min.js" integrity="" crossorigin=""></script>
+        
+    <header>
+
+        <a  href=""  class = "Logo">
+        <img src="assets/img/Logo1.png" alt="Logo Leblanc" class="Logo-img">
+        <h2 class="Nombre-empresa"> Leblanc </h2>
+        </a>
+
+        <nav>
+           <a href="" class="nav-link">Cafes de especialidad</a>
+           <a href="" class="nav-link">Cafeteras</a>
+           <a href="" class="nav-link">Accesorios</a>
+        </nav>
+
+    </header>
+    <h1 style= "color:white"> Leblanc </h1> 
+        <p  style= "color:white"> Somos una empresa dedicada a brindar los mejores recursos para cafeterias o los amantes del cafe.
+        Dale un toque unico a tu pasion con los articulos de Leblanc lo elegante y hogareño es lo que nos define</p>
+    </body>
+</html>
