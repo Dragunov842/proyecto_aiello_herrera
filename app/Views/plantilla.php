7a9766ae585987404e4618aa25a9fefa4f1ec62f
Dragunov <magorros@gmail.com>
2025-04-23 21:59:10 -0300

    Estructura de proyecto completa

--- app/Views/plantilla.php
+++ app/Views/plantilla.php
@@ -22,22 +22,71 @@
     <body>
         <script src="assets/js/bootstrap.bundle.min.js" integrity="" crossorigin=""></script>
         
+
     <header>
 
         <a  href=""  class = "Logo">
         <img src="assets/img/Logo1.png" alt="Logo Leblanc" class="Logo-img">
-        <h2 class="Nombre-empresa"> Leblanc </h2>
+        <h1 class="Nombre-empresa"> Leblanc </h1>
         </a>
 
-        <nav>
-           <a href="" class="nav-link">Cafes de especialidad</a>
-           <a href="" class="nav-link">Cafeteras</a>
-           <a href="" class="nav-link">Accesorios</a>
+        <nav class= "navbar">
+        <a href="" class="nav-link">Cafes de especialidad</a>
+        <a href="" class="nav-link"> Cafeteras</a>
+        <a href="" class="nav-link" >Accesorios</a>
+        <a href="" class="nav-link">Sobre nosotros </a>
         </nav>
+        </header>
 
-    </header>
-    <h1 style= "color:white"> Leblanc </h1> 
-        <p  style= "color:white"> Somos una empresa dedicada a brindar los mejores recursos para cafeterias o los amantes del cafe.
-        Dale un toque unico a tu pasion con los articulos de Leblanc lo elegante y hogareño es lo que nos define</p>
+    <section class="content">
+        <h2 class="title" style= "color:white"> Nuestros productos </h2>
+
+        <div class="box container"> 
+
+        <div class="box">
+            <a href="" class = "Logo">
+                <img src= "assets/img/cafes.jpg" alt="Iconos" class="Logo-img">
+                <p style= "color: white"> Cafes de especialidad </p>
+            </a>
+        </div>
+
+        <div class="box">
+            <a href="" class = "Logo">
+                <img src= "assets/img/cafeteras1.png" alt="Iconos" class="Logo-img">
+                <p style= "color: white"> Cafeteras </p>
+            </a>
+        </div>
+
+        <div class="box">
+            <a href="" class = "Logo">
+                <img src= "assets/img/accesorios.png" alt="Iconos" class="Logo-img">
+                <p style= "color: white"> Accesorios </p>
+            </a>
+        </div>
+
+    </div>
+    
+    </section>
+
+    <section class="aboutus">
+    <h2 class="title" style= "color:white"> Sobre Nosotros </h2>
+    <p  style= "color:white"> 
+        Somos una empresa dedicada a brindar los mejores recursos para cafeterias o los amantes del cafe.
+        Dale un toque unico a tu pasion con los articulos de Leblanc lo elegante y hogareño es lo que nos define
+    </p>
+    <a href="" class="nav-link" style ="color:white" > Conocer mas </a>
+    </section>
+
+    <section class="contacto">
+    <h2 class="title" style= "color:white"> Contactenos </h2>
+    <p  style= "color:white"> 
+        3794-#######
+    </p>
+    <a href="https://www.google.com/maps/place/CAFETERÍA+LE+BLANC/@-33.4376412,-70.6577558,17z/data=!3m1!4b1!4m6!3m5!1s0x9662c52791d79bf1:0x1db1366c986a860e!8m2!3d-33.4376412!4d-70.6551755!16s%2Fg%2F11vwj28xrt?entry=ttu&g_ep=EgoyMDI1MDQyMC4wIKXMDSoASAFQAw%3D%3D" class="mapa">
+        <img src="assets/img/mapa.png" alt="Mapa" height= "auto" width= "300px" class="Mapa">
+    </a>
+
+
+
     </body>
 </html>
